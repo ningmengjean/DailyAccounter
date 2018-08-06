4fef91c5bc64348c7cdfbf2c5eff19e775f52ed4
ningmengjean <ningmengjean@gmail.com>
2018-08-06 20:07:21 +0800

    Initial Commit

---
+++ DailyAccounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyAccounter
+//
+//  Created by wangchi on 2018/8/6.
+//  Copyright © 2018年 Zhu xiaojin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
